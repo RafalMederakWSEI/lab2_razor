b60e2b1ad2ca3fe0b5e3fd7f8590bbe220fe2561
RafalMederakWSEI <rafal.mederak@microsoft.wsei.edu.pl>
2023-06-11 17:33:15 +0200

    Part 8

--- lab2_razor/Models/Post.cs
+++ lab2_razor/Models/Post.cs
@@ -5,8 +5,18 @@ namespace lab2_razor.Pages.Models
     public class Post
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string? Title { get; set; }
+
+        [StringLength(1000, MinimumLength = 3)]
+        [Required]
         public string? Description { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "You can only use letters.")]
         public string? Author { get; set; }
     }
 }
